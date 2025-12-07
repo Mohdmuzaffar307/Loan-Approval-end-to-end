7b44776c5cd78fcfae5f4f104eeca5c51abb437d
Mohdmuzaffar307 <you@example.com>
2025-12-07 13:31:32 +0530

    add app.py in ci workflow

--- app/app.py
+++ app/app.py
@@ -1,13 +1,55 @@
 import pandas as pd
 import joblib
 import streamlit as st
+import mlflow
+import os
+
+
+dagshub_username = os.getenv("DAGSHUB_USERNAME")
+dagshub_token = os.getenv("DAGSHUB_PAT")
+
+if not dagshub_username or not dagshub_token:
+    raise EnvironmentError(
+        "DAGSHUB_USERNAME or DAGSHUB_TOKEN environment variable is not set"
+    )
+
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_username
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+
+dagshub_url = "https://dagshub.com"
+repo_owner = "Mohdmuzaffar307"
+repo_name = "Loan-Approval-end-to-end"
+
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+
+# Load model as a PyFuncModel.
+
+def get_latest_model_version(model_name):
+    client = mlflow.MlflowClient()
+    latest_version = client.get_latest_versions(model_name, stages=["Production"])
+    if not latest_version:
+        latest_version = client.get_latest_versions(model_name, stages=["None"])
+    return latest_version[0].version if latest_version else None
+
+model_name = "my_model"
+model_version = get_latest_model_version(model_name)
+
+model_uri = f'models:/{model_name}/{model_version}'
+model = mlflow.pyfunc.load_model(model_uri)
+
+
+
+#Streamlit app code
 
 st.sidebar.title("Loan Approval")
 st.sidebar.header("Parameters")
 st.sidebar.markdown("Adjust the parameters below:")
 
 preprocessor = joblib.load(open(r"artifacts\model\preprocessor.joblib", "rb"))
-model = joblib.load(open(r"artifacts\model\model.joblib", "rb"))
+# model = joblib.load(open(r"artifacts\model\model.joblib", "rb"))
+model = mlflow.pyfunc.load_model(model)
 
 st.title('Loan Approval App')
 city = st.sidebar.text_input('Enter City', "")
